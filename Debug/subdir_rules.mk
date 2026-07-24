e8ba718524098943ac604c0e607c78fdf3cd3a39
Veki_Wang <1526302178@qq.com>
2026-07-24 15:36:44 +0800

    重新初次提交

---
+++ Debug/subdir_rules.mk
@@ -0,0 +1,39 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Arm Compiler - building file: "$<"'
+	"D:/Download/Microsoft Edge/CCS/ccs/tools/compiler/ti-cgt-armllvm_4.0.4.LTS/bin/tiarmclang.exe" -c @"device.opt"  -march=thumbv6m -mcpu=cortex-m0plus -mfloat-abi=soft -mlittle-endian -mthumb -O2 -I"D:/study/competition/26elCompeetionprepare/ti/TI-NRF24L01-Package" -I"D:/study/competition/26elCompeetionprepare/ti/TI-NRF24L01-Package/Debug" -I"C:/TI/mspm0_sdk_2_11_00_07/source/third_party/CMSIS/Core/Include" -I"C:/TI/mspm0_sdk_2_11_00_07/source" -g -Wall -MMD -MP -MF"$(basename $(<F)).d_raw" -MT"$(@)"  $(GEN_OPTS__FLAG) -o"$@" "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+build-634255875: ../empty.syscfg
+	@echo 'SysConfig - building file: "$<"'
+	"C:/TI/sysconfig_1.26.2/sysconfig_cli.bat" -s "C:/TI/mspm0_sdk_2_11_00_07/.metadata/product.json" --script "D:/study/competition/26elCompeetionprepare/ti/TI-NRF24L01-Package/empty.syscfg" -o "." --compiler ticlang
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+device_linker.cmd: build-634255875 ../empty.syscfg
+device.opt: build-634255875
+device.cmd.genlibs: build-634255875
+ti_msp_dl_config.c: build-634255875
+ti_msp_dl_config.h: build-634255875
+Event.dot: build-634255875
+
+%.o: ./%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Arm Compiler - building file: "$<"'
+	"D:/Download/Microsoft Edge/CCS/ccs/tools/compiler/ti-cgt-armllvm_4.0.4.LTS/bin/tiarmclang.exe" -c @"device.opt"  -march=thumbv6m -mcpu=cortex-m0plus -mfloat-abi=soft -mlittle-endian -mthumb -O2 -I"D:/study/competition/26elCompeetionprepare/ti/TI-NRF24L01-Package" -I"D:/study/competition/26elCompeetionprepare/ti/TI-NRF24L01-Package/Debug" -I"C:/TI/mspm0_sdk_2_11_00_07/source/third_party/CMSIS/Core/Include" -I"C:/TI/mspm0_sdk_2_11_00_07/source" -g -Wall -MMD -MP -MF"$(basename $(<F)).d_raw" -MT"$(@)"  $(GEN_OPTS__FLAG) -o"$@" "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+startup_mspm0g351x_ticlang.o: C:/TI/mspm0_sdk_2_11_00_07/source/ti/devices/msp/m0p/startup_system_files/ticlang/startup_mspm0g351x_ticlang.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Arm Compiler - building file: "$<"'
+	"D:/Download/Microsoft Edge/CCS/ccs/tools/compiler/ti-cgt-armllvm_4.0.4.LTS/bin/tiarmclang.exe" -c @"device.opt"  -march=thumbv6m -mcpu=cortex-m0plus -mfloat-abi=soft -mlittle-endian -mthumb -O2 -I"D:/study/competition/26elCompeetionprepare/ti/TI-NRF24L01-Package" -I"D:/study/competition/26elCompeetionprepare/ti/TI-NRF24L01-Package/Debug" -I"C:/TI/mspm0_sdk_2_11_00_07/source/third_party/CMSIS/Core/Include" -I"C:/TI/mspm0_sdk_2_11_00_07/source" -g -Wall -MMD -MP -MF"$(basename $(<F)).d_raw" -MT"$(@)"  $(GEN_OPTS__FLAG) -o"$@" "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+
